777a9a852702815ef0c3d91d0185c6fa2f161e42
1dupuis <uaintscammingme2@gmail.com>
2024-11-07 18:13:14 -0500

    Update new-user-intro.js

--- new-user-intro.js
+++ new-user-intro.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebas
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail, GoogleAuthProvider, signInWithPopup, signOut } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 import { getDatabase, ref, set, get } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
 
-const NewUserIntro = {
+class NewUserIntro {
 
     constructor() {
         this.config = {
