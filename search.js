889f879e8b2ec82732aad19752e40f62f00fb6eb
1dupuis <uaintscammingme2@gmail.com>
2024-12-09 08:15:50 -0500

    Update search.js

--- search.js
+++ search.js
@@ -44,6 +44,7 @@ const config = {
                {
                    name: '❇️ AI',
                    items: [
+                       { name: 'GameGenie', url: 'https://gg.dupuis.lol', description: 'Google Gemini game maker' },
                        { name: 'Make or Break', url: 'https://mob.dupuis.lol', description: 'Google Gemini business game' },
                        { name: 'ChatDupuis', url: 'https://chat.dupuis.lol', description: 'Google Gemini French tutor' }
                    ]
