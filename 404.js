4cd85aed738687cefa77b4ac58df495fd510264f
1dupuis <uaintscammingme2@gmail.com>
2024-12-16 08:03:51 -0500

    Update 404.js

--- 404.js
+++ 404.js
@@ -1,15 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     const errorDetails = document.getElementById("error-details");
 
-    // Display URL of the page that caused the error
     if (window.location) {
         errorDetails.innerHTML = `<p>The URL that caused this error was: <strong>${window.location.href}</strong></p>`;
     }
 
-    // Change reload button functionality to go to home
     const reloadBtn = document.getElementById("reload-btn");
     reloadBtn.addEventListener("click", () => {
-        // Redirect to the homepage
         window.location.href = '/';
     });
 });
