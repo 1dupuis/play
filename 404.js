3a198da1434e62e2bcba14f5b5bf45cb0626b0b4
1dupuis <uaintscammingme2@gmail.com>
2024-12-17 19:20:51 -0500

    Update 404.js

--- 404.js
+++ 404.js
@@ -1,12 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const errorDetails = document.getElementById("error-details");
+//document.addEventListener("DOMContentLoaded", () => {
+    //const errorDetails = document.getElementById("error-details");
 
-    if (window.location) {
-        errorDetails.innerHTML = `<p>The URL that caused this error was: <strong>${window.location.href}</strong></p>`;
-    }
+    //if (window.location) {
+    //    errorDetails.innerHTML = `<p>The URL that caused this error was: <strong>${window.location.href}</strong></p>`;
+    //}
 
-    const reloadBtn = document.getElementById("reload-btn");
-    reloadBtn.addEventListener("click", () => {
-        window.location.href = '/';
-    });
-});
+    //const reloadBtn = document.getElementById("reload-btn");
+    //reloadBtn.addEventListener("click", () => {
+    //    window.location.href = '/';
+    //});
+//});
